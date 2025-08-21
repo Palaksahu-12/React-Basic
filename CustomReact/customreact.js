51c023a8d11eb1a9b92581038ad9acf6fe2c9c93
Palak Sahu <palaksahu2233@gmail.com>
2025-08-21 19:16:51 +0530

    Update customreact.js

--- CustomReact/customreact.js
+++ CustomReact/customreact.js
@@ -1,9 +1,4 @@
 function customRender(reactElement , container){
-    // const domElement = document.createElement(reactElement.type)
-    // domElement.innerHTML = reactElement.children
-    // domElement.setAttribute('href', reactElement.props.href)
-    // domElement.setAttribute('target', reactElement.props.target)
-    // container.appendChild(domElement)
 
     const domElement = document.createElement(reactElement.type); 
     domElement.innerHTML = reactElement.children;
@@ -25,5 +20,6 @@ const reactElement = {
 }
 
 const mainContainer = document.querySelector('#root');
+
 
 customRender(reactElement , mainContainer)
